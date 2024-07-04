3add7679fd46d68c1a118aa7476f37bf7405d467
arasruislam <arasru01@gmail.com>
2024-07-04 17:59:36 +0600

    All Done

--- task/urls.py
+++ task/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("task/", views.Task, name="task"),
+    path("edit/<int:id>", views.EditTask, name="EditTask"),
+    path("delete/<int:id>", views.DeleteTask, name="DeleteTask"),
 ]
--- task/views.py
+++ task/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from . import forms
+from . import forms, models
 
 
 # Create your views here.
@@ -12,3 +12,22 @@ def Task(request):
     else:
         taskForm = forms.TaskForm()
     return render(request, "addTask.html", {"form": taskForm})
+
+
+def EditTask(request, id):
+    task = models.Task_Model.objects.get(pk=id)
+
+    taskForm = forms.TaskForm(instance=task)
+    if request.method == "POST":
+        taskForm = forms.TaskForm(request.POST, instance=task)
+        if taskForm.is_valid():
+            taskForm.save()
+            return redirect("homepage")
+
+    return render(request, "addTask.html", {"form": taskForm})
+
+
+def DeleteTask(request, id):
+    task = models.Task_Model.objects.get(pk=id)
+    task.delete()
+    return redirect("homepage")
