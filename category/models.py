3df29415b67eda01ab10f7bd13228388e6316b07
arasruislam <arasru01@gmail.com>
2024-07-04 10:04:04 +0600

    model creation done

--- category/models.py
+++ category/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self) -> str:
+        return self.name
--- task/models.py
+++ task/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from category.models import CategoryModel
+
 
 # Create your models here.
+class Task_Model(models.Model):
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    is_completed = models.BooleanField()
+    Task_Assign_Data = models.DateField()
+
+    # relation with category => many to many relation
+    category = models.ManyToManyField(CategoryModel)
+
+    def __str__(self) -> str:
+        return self.title
